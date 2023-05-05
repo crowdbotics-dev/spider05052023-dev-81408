28d86c5a708b6dd1aada3b225d33385f83ef21fd
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 10:07:31 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,260 +1,364 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Text,
   View,
-  StyleSheet,
-  Pressable,
-  ImageBackground,
-  Image,
-  FlatList
+  StyleSheet, Image, TextInput, ScrollView
 } from "react-native";
 
-const FoodCategory = () => {
-  const [selectedTab1, setSelectedTab1] = useState(0);
-  const [selectedTab2, setSelectedTab2] = useState(0);
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    setCategories([
-      {
-        id: 1,
-        name: "Burger",
-        image: require("./assets/itemImage.png")
-      },
-      {
-        id: 2,
-        name: "Pizza",
-        image: require("./assets/itemImage2.png")
-      },
-      {
-        id: 3,
-        name: "Chinese",
-        image: require("./assets/itemImage.png")
-      }
-    ]);
-  }, []);
+const TransactionsFilterList = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.body}>
-        <TabView
-          tabTitles={["Delivery", "Pickup"]}
-          selected={selectedTab1}
-          onPress={x => {
-            setSelectedTab1(x);
-          }}
-          style={styles.header}
-        />
-        <ImageBackground
-          style={styles.bannerImage}
-          source={require("./assets/bannerImage.png")}>
-          <View style={styles.bannerTitleContainer}>
-            <Text style={styles.bannerText}>Discover</Text>
-            <Text style={styles.bannerTitle}>Best Dinner of the day</Text>
+    <ScrollView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <View style={styles.header}>
+          <Image source={require("./assets/back.png")} style={styles.back} />
+          <Text style={styles.heading}>Transaction</Text>
+          <Text />
+        </View>
+        <View style={styles.statusContainer}>
+          <View style={styles.imgContainer2}>
+            <Image source={require(
+              // @ts-ignore
+              "./assets/edit.png")} style={styles.editImg} />
           </View>
-          <Image
-            style={styles.arrowIcon}
-            source={require("./assets/arrowIcon.png")}
-          />
-        </ImageBackground>
-        <Text style={styles.heading}>Filter</Text>
-        <TabView
-          tabTitles={["Nearby", "Above 4.5", "Cheapest"]}
-          icons={[
-            require("./assets/locationIcon.png"),
-            require("./assets/starIcon.png"),
-            require("./assets/tagIcon.png")
-          ]}
-          selected={selectedTab2}
-          onPress={x => {
-            setSelectedTab2(x);
-          }}
-          backgroundColor="#fff"
-          style={styles.tabView}
-        />
+          <View style={styles.description}>
+            <Text style={styles.username}>$1244.84</Text>
+            <Text style={styles.text}>Balance</Text>
+          </View>
+        </View>
       </View>
-      <View style={styles.categoriesContainer}>
-        <Text style={styles.categoryTitle}>Category</Text>
-        <FlatList
-          data={categories}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => (
-            <ImageBackground style={styles.categoryImage} source={item.image}>
-              <Text style={styles.categoryText}>{item.name}</Text>
-            </ImageBackground>
-          )}
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-        />
+      <Text style={styles.mrSearch}>Search</Text>
+      <View style={styles.searchContainer}>
+        <View style={styles.chooseContainer}>
+          <TextInput placeholder="Enter" placeholderTextColor="#000" />
+          <Image source={require(
+            // @ts-ignore
+            "./assets/search.png")} style={styles.search} />
+        </View>
+        <Image source={require(
+          // @ts-ignore
+          "./assets/file.png")} style={styles.file} />
       </View>
-    </View>
+
+      <View style={styles.dateContainer}>
+      <Image source={require(
+        // @ts-ignore
+        "./assets/cross.png")} style={styles.cross} />
+        <View style={styles.feeContainer}>
+          <View>
+            <Text style={[styles.mr10]}>Start date</Text>
+            <View style={styles.feeSection}>
+              <TextInput placeholder="Enter" placeholderTextColor="#000"/>
+              <Image source={require(
+                // @ts-ignore
+                "./assets/calender.png")} style={styles.calender} />
+            </View>
+          </View>
+          <View>
+            <Text style={[styles.mr10]}>End date</Text>
+            <View style={styles.feeSection}>
+            <TextInput placeholder="Enter" placeholderTextColor="#000"/>
+              <Image source={require(
+                // @ts-ignore
+                "./assets/calender.png")} style={styles.calender} />
+            </View>
+          </View>
+        </View>
+        <Text style={styles.mr10}>Transactions</Text>
+        <View style={[styles.chooseContainer, styles.searchTo]}>
+          <Text>All</Text>
+          <Image source={require(
+            // @ts-ignore
+            "./assets/down.png")} style={styles.down} />
+        </View>
+      </View>
+
+      <View style={styles.tabView}>
+        <View style={[styles.tabItem, styles.selectedTab]}>
+          <Text style={styles.tabText}>Loads</Text>
+        </View>
+        <View style={styles.tabItem}>
+          <Text style={[styles.tabText]}>Cash out</Text>
+        </View>
+      </View>
+      <Text style={styles.subHeading}>Transactions</Text>
+      <View style={styles.walletCard}>
+        <View style={styles.walletInner}>
+          <View style={[styles.imgContainer]}>
+            <Image
+              source={require("./assets/edit.png")}
+              style={styles.image}
+            />
+          </View>
+          <View style={styles.walletCarder}>
+            <Text style={styles.eventName}>Sender name</Text>
+            <Text style={styles.eventType}>07/06/2022</Text>
+          </View>
+        </View>
+        <View style={styles.leftSection}>
+          <Text style={styles.view}>11:30 am</Text>
+          <Text style={styles.reject}>1.254.21$</Text>
+        </View>
+      </View>
+
+      <View style={styles.walletCard}>
+        <View style={styles.walletInner}>
+          <View style={[styles.imgContainer]}>
+            <Image
+              source={require("./assets/edit.png")}
+              style={styles.image}
+            />
+          </View>
+
+          <View style={styles.walletCarder}>
+            <Text style={styles.eventName}>Sender name</Text>
+            <Text style={styles.eventType}>07/06/2022</Text>
+          </View>
+        </View>
+        <View style={styles.leftSection}>
+          <Text style={styles.view}>11:30 am</Text>
+          <Text style={styles.reject}>1.254.21$</Text>
+        </View>
+      </View>
+      <View style={styles.walletCard}>
+        <View style={styles.walletInner}>
+          <View style={[styles.imgContainer]}>
+            <Image
+              source={require("./assets/edit.png")}
+              style={styles.image}
+            />
+          </View>
+
+          <View style={styles.walletCarder}>
+            <Text style={styles.eventName}>Sender name</Text>
+            <Text style={styles.eventType}>07/06/2022</Text>
+          </View>
+        </View>
+        <View style={styles.leftSection}>
+          <Text style={styles.view}>11:30 am</Text>
+          <Text style={styles.reject}>1.254.21$</Text>
+        </View>
+      </View>
+      <View style={styles.walletCard}>
+        <View style={styles.walletInner}>
+          <View style={[styles.imgContainer]}>
+            <Image
+              source={require("./assets/edit.png")}
+              style={styles.image}
+            />
+          </View>
+
+          <View style={styles.walletCarder}>
+            <Text style={styles.eventName}>Sender name</Text>
+            <Text style={styles.eventType}>07/06/2022</Text>
+          </View>
+        </View>
+        <View style={styles.leftSection}>
+          <Text style={styles.view}>11:30 am</Text>
+          <Text style={styles.reject}>1.254.21$</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#FFF",
+    paddingBottom: 20
   },
-  body: {
-    paddingHorizontal: 20
-  },
+  headerContainer: { backgroundColor: "#F1F1F1", paddingHorizontal: 10, paddingBottom: 50, marginBottom: 20 },
   header: {
-    width: "70%"
-  },
-  bannerImage: {
-    width: "100%",
-    height: 200,
-    borderRadius: 15,
-    overflow: "hidden",
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    elevation: 10,
-    marginTop: 20
-  },
-  bannerTitleContainer: {
-    position: "absolute",
-    top: 15,
-    left: 15
-  },
-  bannerText: {
-    fontSize: 14,
-    color: "#77838F",
-    fontWeight: "bold"
-  },
-  bannerTitle: {
-    fontSize: 18,
-    color: "#1E2022",
-    fontWeight: "bold",
-    width: 100
-  },
-  arrowIcon: {
-    position: "absolute",
-    bottom: 15,
-    right: 15,
-    width: 20,
-    height: 16,
-    resizeMode: "contain"
-  },
-  heading: {
-    fontSize: 14,
-    marginTop: 20,
-    marginLeft: 20
-  },
-  tabView: {
-    borderColor: "#c1c1c1",
-    borderWidth: 1,
-    padding: 5
-  },
-  categoryImage: {
-    width: 150,
-    height: 200,
-    borderRadius: 15,
-    overflow: "hidden",
-    marginHorizontal: 10
-  },
-  categoryText: {
-    fontSize: 14,
-    color: "#1E2022",
-    fontWeight: "bold",
-    position: "absolute",
-    top: 15,
-    left: 15
-  },
-  categoryTitle: {
-    fontSize: 16,
-    color: "#1E2022",
-    fontWeight: "bold",
-    marginVertical: 20,
-    marginLeft: 20
-  }
-});
-
-export default FoodCategory;
-
-const TabView = ({
-  tabTitles,
-  selected,
-  onPress,
-  tabColor,
-  backgroundColor,
-  style,
-  icons
-}) => {
-  const tabColorStyle = {
-    backgroundColor: tabColor || "#fff"
-  };
-  const backgroundColorStyle = {
-    backgroundColor: backgroundColor || "#F1F1F1"
-  };
-  const propStyle = style || {};
-  return (
-    <View
-      style={[tabViewStyles.paletteContainer, backgroundColorStyle, propStyle]}>
-      {tabTitles.map((title, index) => (
-        <Pressable
-          onPress={() => (onPress ? onPress(index) : null)}
-          style={
-            index === selected
-              ? [tabViewStyles.selected, tabColorStyle, tabViewStyles.tabItem]
-              : [
-                  tabViewStyles.unSelected,
-                  backgroundColorStyle,
-                  tabViewStyles.tabItem
-                ]
-          }
-          key={index}>
-          {icons
-            ? (
-            <Image
-              source={icons[index]}
-              style={[
-                tabViewStyles.icon,
-                index === selected
-                  ? tabViewStyles.selectedIcon
-                  : tabViewStyles.unSelectedIcon
-              ]}
-            />
-              )
-            : null}
-          <Text>{title}</Text>
-        </Pressable>
-      ))}
-    </View>
-  );
-};
-
-const tabViewStyles = StyleSheet.create({
-  paletteContainer: {
-    height: 48,
-    backgroundColor: "#E4E4E4",
     flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    borderRadius: 10,
-    padding: 6,
+    marginHorizontal: 30,
+    marginTop: 15,
+    marginBottom: 20
+  },
+  back: { width: 11.25, height: 20, resizeMode: "contain", marginLeft: -15 },
+  heading: { fontSize: 16, color: "#000" },
+  description: { marginTop: 10, marginRight: 10 },
+  username: { fontSize: 20, fontWeight: "bold" },
+  text: { color: "#9A9A9A", marginTop: 5, marginLeft: 35 },
+  statusContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginVertical: 10
   },
-  tabItem: {
-    borderRadius: 10,
-    flex: 1,
-    height: "100%",
+  imgContainer2: {
+    height: 61,
+    width: 61,
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "row"
+    backgroundColor: "#DADADA",
+    borderRadius: 60,
+    marginLeft: 20
   },
-  selected: {
+  editImg: { resizeMode: "contain", height: 32, width: 32 },
+  tabView: {
+    width: "65%",
+    height: 48,
+    backgroundColor: "#F1F1F1",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderRadius: 10,
+    paddingVertical: 6,
+    paddingHorizontal: 6,
+    marginVertical: 30,
+    marginLeft: 10
+  },
+  tabItem: {
+    height: "100%",
+    paddingHorizontal: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F1F1F1",
+    borderRadius: 10,
+    fontWeight: "bold",
+    flex: 1
+  },
+  selectedTab: {
+    backgroundColor: "#FFF",
     shadowColor: "gray",
     elevation: 10
   },
-  unSelected: {
-    backgroundColor: "#f1f1f1"
+  tabText: { fontSize: 12 },
+  subHeading: { fontSize: 16, marginLeft: 13, marginVertical: 10 },
+  walletCard: {
+    backgroundColor: "#fff",
+    marginHorizontal: 10,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottomColor: "#F2F2F2",
+    borderBottomWidth: 1,
+    paddingVertical: 10
   },
-  icon: {
+  walletInner: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  walletCarder: {
+    alignSelf: "center",
+    display: "flex",
+    flexDirection: "column"
+  },
+  eventName: {
+    fontSize: 14,
+    marginLeft: 10,
+    width: 115
+  },
+  eventType: {
+    color: "#ADB1B2",
+    fontSize: 12,
+    marginLeft: 10,
+    width: 115,
+    marginVertical: 5
+  },
+  view: {
+    fontSize: 12,
+    color: "#ADB1B2",
+    paddingBottom: 3,
+    marginRight: -25
+  },
+  reject: {
+    fontSize: 18,
+    paddingHorizontal: 10
+  },
+  imgContainer: {
+    height: 46,
+    width: 46,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#dadada",
+    borderRadius: 10
+  },
+  image: { resizeMode: "contain", height: 24, width: 24 },
+  leftSection: { justifyContent: "center", alignItems: "center" },
+  mr10: {
+    marginLeft: 15,
+    marginBottom: 10
+  },
+  mrSearch: {
+    marginLeft: 35,
+    marginBottom: 10
+  },
+  chooseContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 55,
+    borderColor: "#C4C4C4",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 15,
+    paddingRight: 20,
+    marginBottom: 10,
+    width: "83%"
+  },
+  searchTo: { width: "100%" },
+  amount: { fontSize: 20, fontWeight: "bold" },
+  search: {
     width: 20,
-    height: 20,
+    height: 24,
+    resizeMode: "contain"
+  },
+  down: {
+    width: 12,
+    height: 17,
+    resizeMode: "contain"
+  },
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20
+  },
+  file: {
+    width: 24,
+    height: 24,
     resizeMode: "contain",
-    marginRight: 5
+    marginTop: -10
   },
-  selectedIcon: {
-    tintColor: "#000"
+  calender: {
+    width: 24,
+    height: 24,
+    resizeMode: "contain"
   },
-  unSelectedIcon: {
-    tintColor: "#7C7C7C"
+  dateContainer: {
+    elevation: 5,
+    shadowColor: "#ccc9c9",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    display: "flex",
+    borderRadius: 8,
+    marginHorizontal: 10,
+    marginTop: 10
+  },
+  feeContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginBottom: 10 },
+  cross: {
+    width: 16,
+    height: 16,
+    resizeMode: "contain",
+    alignSelf: "flex-end",
+    marginBottom: -15
+  },
+  feeSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 55,
+    borderColor: "#C4C4C4",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+    width: 150
   }
 });
+
+export default TransactionsFilterList;
