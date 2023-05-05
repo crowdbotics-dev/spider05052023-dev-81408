e4ed4c002458d0ae083c750463de5698a49e9de1
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 10:08:17 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,99 +1,59 @@
-import React from "react";
-import { Text, StyleSheet, View, Image, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ScrollView, ActivityIndicator, Text } from "react-native";
+import { getUserById, slice } from "./store";
+import { useSelector, useDispatch } from "react-redux";
+import { styles, Color } from "./styles";
+import { unwrapResult } from "@reduxjs/toolkit";
+import { EditUser } from "./edit";
+import ViewUser from "./view";
 
-const UploadMediaScreen = (params) => {
+export const UserProfile = ({ route }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  // code below depends on the existence of any login module - update as needed.
+  const login = useSelector(state => {
+    return state?.login;
+  });
+  const userId = route.params?.id || login?.user.id;
+  const user = useSelector(state => state.userProfile.users[userId]);
+  const api = useSelector(state => state.userProfile.api);
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    if (userId) {
+      dispatch(getUserById(userId))
+        .then(unwrapResult)
+        .then(response => {
+          const edit = response.id === login?.user.id;
+          setIsEdit(edit);
+        })
+        .catch(e => console.log(e));
+    }
+  }, [userId]);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.menu}>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Take photo or video</Text>
-          <Image source={require("./assets/cameraIcon.png")} />
+    <ScrollView style={styles.container} contentStyle={styles.content}>
+      {api.loading === "pending"
+        ? (
+        <View>
+          <ActivityIndicator color={Color.steel} />
         </View>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Photo Library</Text>
-          <Image source={require("./assets/libraryIcon.png")} />
+          )
+        : (
+        <View>
+          <View>{!user && <Text>No user to display information.</Text>}</View>
+          {user && (
+            <View>
+              {isEdit ? <EditUser user={user} /> : <ViewUser user={user} />}
+            </View>
+          )}
         </View>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Browse</Text>
-          <Image source={require("./assets/menuIcon.png")} />
-        </View>
-      </View>
-      <Button buttonText="Cancel" outline={true} />
-    </View>
+          )}
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 20,
-    justifyContent: "flex-end"
-  },
-  menuItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-    justifyContent: "space-between",
-    borderBottomWidth: 1,
-    borderColor: "#ccc"
-  },
-  menuItemText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "left"
-  }
-});
-export default UploadMediaScreen;
-
-const Button = (params) => {
-  const btnStyle = {
-    backgroundColor: params.outline ? "#fff" : "#000",
-    borderColor: params.outline ? "#000" : "#fff",
-    borderWidth: 1
-  };
-  const btnText = {
-    color: params.outline ? "#000" : "#fff"
-  };
-  return (
-    <View style={buttonStyles.btnContainer}>
-      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
-        <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
-        <View style={styles.childrenContainer}>{params.children}</View>
-      </Pressable>
-    </View>
-  );
+export default {
+  title: "userProfile",
+  navigator: UserProfile,
+  slice
 };
-
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    paddingTop: 10,
-    paddingHorizontal: 20,
-    justifyContent: "center",
-    marginTop: 20
-  },
-  btn: {
-    backgroundColor: "black",
-    height: 50,
-    width: "100%",
-    padding: 10,
-    paddingHorizontal: 25,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "rgba(0, 0, 0, 0.2)",
-    elevation: 10,
-    flexDirection: "row"
-  },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
