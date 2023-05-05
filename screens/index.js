57ae2d523541d930de8dd91b9e644fe325ad2de0
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 10:08:27 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,193 +1,98 @@
 import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  Pressable,
-  TextInput
-} from "react-native";
+import { Text, StyleSheet, View, FlatList, Image } from "react-native";
 
-const VideoCallDetails = () => {
-  const [selectedTab, setSelectedTab] = useState(0);
-  const [meetingDetails, setMeetingDetails] = useState({});
+const ProductListingScreen = (params) => {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    setMeetingDetails({
-      title: "Sales Strategy Session",
-      pin: 45620,
-      url: "https://crowdbotics/abc.test-123"
-    });
+    setProducts([
+      {
+        id: 1,
+        name: "Product name",
+        status: true,
+        isFavorite: false,
+        image: require("./assets/productImage.png")
+      },
+      {
+        id: 2,
+        name: "Product name",
+        status: false,
+        isFavorite: true,
+        image: require("./assets/productImage2.png")
+      },
+      {
+        id: 3,
+        name: "Product name",
+        status: true,
+        isFavorite: false,
+        image: require("./assets/productImage.png")
+      },
+      {
+        id: 4,
+        name: "Product name",
+        status: false,
+        isFavorite: true,
+        image: require("./assets/productImage2.png")
+      },
+      {
+        id: 5,
+        name: "Product name",
+        status: true,
+        isFavorite: false,
+        image: require("./assets/productImage.png")
+      },
+      {
+        id: 6,
+        name: "Product name",
+        status: false,
+        isFavorite: true,
+        image: require("./assets/productImage2.png")
+      }
+    ]);
   }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.roundIconContainer}>
-          <Image
-            source={require("./assets/videoIcon.png")}
-            style={styles.icon}
-          />
-        </View>
-        <View style={styles.roundIconContainer}>
-          <Image
-            source={require("./assets/voiceIcon.png")}
-            style={styles.icon}
-          />
-        </View>
-        <View style={styles.roundIconContainer}>
-          <Image
-            source={require("./assets/phoneIcon.png")}
-            style={styles.icon}
-          />
-        </View>
-        <View style={[styles.roundIconContainer, styles.redBg]}>
-          <Image
-            source={require("./assets/callIcon.png")}
-            style={styles.icon}
-          />
-        </View>
-      </View>
-      <TabView
-        tabTitles={["People", "Details"]}
-        selected={selectedTab}
-        onPress={setSelectedTab}
-        style={styles.tabView}
-      />
-      <View style={styles.separator}>
-        <Text style={styles.separatorText}>{meetingDetails.title}</Text>
-      </View>
-      <Input
-        value={meetingDetails.url}
-        editable={false}
-        icon={require("./assets/linkIcon.png")}
-        containerStyle={styles.input}
-      />
-      <Input
-        value={"PIN: " + meetingDetails.pin}
-        editable={false}
-        icon={require("./assets/pinIcon.png")}
-        containerStyle={styles.input}
-      />
-      <Button buttonText="Share Joining Info" style={styles.button}>
-        <Image
-          source={require("./assets/shareIcon.png")}
-          style={styles.btnIcon}
+      <TabView tabTitles={["All", "Best Products"]} selected={0} />
+      <View style={styles.productsContainer}>
+        <FlatList
+          data={products}
+          numColumns={2}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Product product={item} />}
+          columnWrapperStyle={{
+            justifyContent: "space-around"
+          }}
+          showsVerticalScrollIndicator={false}
         />
-      </Button>
+      </View>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
   },
-  heading: {
-    fontSize: 20
-  },
-  header: {
-    justifyContent: "space-around",
-    alignItems: "flex-end",
-    backgroundColor: "#FCF1D6",
-    height: 180,
-    flexDirection: "row",
-    paddingHorizontal: 30,
-    paddingVertical: 20
-  },
-  roundIconContainer: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#fff",
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    resizeMode: "contain"
-  },
-  redBg: {
-    backgroundColor: "#EA4335"
-  },
-  tabView: {
-    marginVertical: 20,
-    marginHorizontal: 20,
-    width: "70%"
-  },
-  separator: {
-    height: 50,
-    backgroundColor: "#DADADA",
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    marginBottom: 20
-  },
-  separatorText: {
-    fontSize: 16
-  },
-  input: {
-    marginHorizontal: 20
-  },
-  button: {
-    marginHorizontal: 20,
-    marginVertical: 20
-  },
-  btnIcon: {
-    width: 20,
-    height: 20,
-    resizeMode: "contain",
-    marginLeft: 10
+  productsContainer: {
+    paddingHorizontal: 20
   }
 });
+export default ProductListingScreen;
 
-export default VideoCallDetails;
-
-const TabView = ({
-  tabTitles,
-  selected,
-  onPress,
-  tabColor,
-  backgroundColor,
-  style,
-  icons
-}) => {
-  const tabColorStyle = {
-    backgroundColor: tabColor || "#fff"
-  };
-  const backgroundColorStyle = {
-    backgroundColor: backgroundColor || "#F1F1F1"
-  };
-  const propStyle = style || {};
+const TabView = ({ tabTitles, selected }) => {
   return (
-    <View
-      style={[tabViewStyles.paletteContainer, backgroundColorStyle, propStyle]}>
+    <View style={tabViewStyles.paletteContainer}>
       {tabTitles.map((title, index) => (
-        <Pressable
-          onPress={() => (onPress ? onPress(index) : null)}
+        <View
           style={
             index === selected
-              ? [tabViewStyles.selected, tabColorStyle, tabViewStyles.tabItem]
-              : [
-                  tabViewStyles.unSelected,
-                  backgroundColorStyle,
-                  tabViewStyles.tabItem
-                ]
+              ? tabViewStyles.selected
+              : tabViewStyles.unSelected
           }
-          key={index}>
-          {icons
-            ? (
-            <Image
-              source={icons[index]}
-              style={[
-                tabViewStyles.icon,
-                index === selected
-                  ? tabViewStyles.selectedIcon
-                  : tabViewStyles.unSelectedIcon
-              ]}
-            />
-              )
-            : null}
+          key={index}
+        >
           <Text>{title}</Text>
-        </Pressable>
+        </View>
       ))}
     </View>
   );
@@ -195,198 +100,104 @@ const TabView = ({
 
 const tabViewStyles = StyleSheet.create({
   paletteContainer: {
+    width: "70%",
     height: 48,
-    backgroundColor: "#E4E4E4",
+    backgroundColor: "#F1F1F1",
     flexDirection: "row",
     alignItems: "center",
     borderRadius: 10,
     padding: 6,
-    marginVertical: 10
+    marginVertical: 10,
+    marginHorizontal: 20
   },
-  tabItem: {
+  selected: {
     borderRadius: 10,
+    flex: 1,
+    backgroundColor: "#fff",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "gray",
+    elevation: 10
+  },
+  unSelected: {
     flex: 1,
     height: "100%",
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "row"
-  },
-  selected: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    elevation: 10,
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 5
-  },
-  unSelected: {
-    backgroundColor: "#f1f1f1"
-  },
-  icon: {
-    width: 20,
-    height: 20,
-    resizeMode: "contain",
-    marginRight: 5
-  },
-  selectedIcon: {
-    tintColor: "#000"
-  },
-  unSelectedIcon: {
-    tintColor: "#7C7C7C"
+    backgroundColor: "#F1F1F1",
+    borderRadius: 10
   }
 });
 
-const Input = props => {
-  return (
-    <View style={[inputStyles.inputContainer, props.containerStyle]}>
-      {props.text
-        ? (
-        <Text style={inputStyles.inputText}>{props.text}</Text>
-          )
-        : null}
-
-      <TextInput
-        style={[
-          inputStyles.input,
-          props.style,
-          props.textArea ? inputStyles.textArea : null
-        ]}
-        placeholder={props.placeholder ? props.placeholder : "Enter"}
-        value={props.value}
-        onChangeText={() => props.onChange()}
-        placeholderTextColor={
-          props.placeholderTextColor ? props.placeholderTextColor : "#9B9B9B"
-        }
-        editable={props.editable !== false}
-        autoCapitalize="none"
-        autoCorrect={false}
-        multiline={!!props.textArea}
-        backgroundColor={props.backgroundColor}
-        secureTextEntry={props.secureTextEntry}
-      />
-      {props.errorText
-        ? (
-        <Text style={inputStyles.error}>{props.errorText}</Text>
-          )
-        : null}
-      {props.icon
-        ? (
-        <Pressable
-          onPress={() => props.iconOnPress()}
-          style={inputStyles.iconWithText}>
-          <Image source={props.icon} style={inputStyles.icon} />
-        </Pressable>
-          )
-        : null}
-      <View style={styles.children}>{props.children}</View>
-    </View>
-  );
-};
-
-const inputStyles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  inputText: {
-    fontSize: 14,
-    marginLeft: 20,
-    color: "#111112"
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
-    borderRadius: 10,
-    padding: 10,
-    paddingLeft: 20,
-    marginVertical: 10,
-    width: "100%",
-    height: 50,
-    color: "#000"
-  },
-  iconWithText: {
-    position: "absolute",
-    right: 30,
-    bottom: 25,
-    width: 20,
-    height: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 1
-  },
-  icon: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain"
-  },
-  textArea: {
-    height: 150
-  },
-  children: {}
-});
-
-const Button = params => {
-  const backgroundColor = params.backgroundColor || "#000";
-  const textColor = params.textColor || "#fff";
-  const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.borderColor || backgroundColor,
-    borderWidth: 1
-  };
-  const btnText = {
-    color: textColor
+const Product = ({ product }) => {
+  const availability = {
+    color: product.status ? "#12D790" : "#EA4335",
+    fontSize: 12,
+    fontWeight: "bold"
   };
   return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
+    <View style={productStyles.container}>
+      <View style={productStyles.imageContainer}>
+        <Image source={product.image} style={productStyles.productImage} />
+
+        <Image
+          source={
+            product.isFavorite
+              ? require("./assets/isFavouriteIcon.png")
+              : require("./assets/favIcon.png")
+          }
+          style={productStyles.favIcon}
+        />
+      </View>
+      <View style={productStyles.descriptionContainer}>
+        <Text style={productStyles.bold}>{product.name}</Text>
+        <View style={productStyles.availabilityTextContainer}>
+          <Text style={productStyles.availabilityText}>Purchase: </Text>
+          <Text style={availability}>
+            {product.status ? "Available" : "Not available"}
           </Text>
-          <View style={styles.childrenContainer}>{params.children}</View>
-        </Pressable>
+        </View>
       </View>
     </View>
   );
 };
 
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    justifyContent: "center"
+const productStyles = StyleSheet.create({
+  container: {
+    height: 240,
+    width: 160,
+    margin: 10
   },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
+  imageContainer: {
+    height: 180,
+    width: 160,
     borderRadius: 10
   },
-  btn: {
-    height: 50,
-    padding: 10,
-    paddingHorizontal: 25,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-
-    flexDirection: "row"
+  productImage: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 10
   },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
+  descriptionContainer: {
+    justifyContent: "center",
+    padding: 10
+  },
+  availabilityTextContainer: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  availabilityText: {
+    color: "#7C7C7C",
+    fontSize: 12,
     fontWeight: "bold"
   },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
+  bold: {
+    fontWeight: "bold"
+  },
+  favIcon: {
+    position: "absolute",
+    right: 10,
+    top: 10
   }
 });
